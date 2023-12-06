05471a35918caeaff49849165ca4ccde49cf3939
정민규 <fastdodge7@naver.com>
2023-12-06 12:21:25 +0900

    mission10 temp

--- src/main/java/com/umc/study/web/dto/ReviewRequestDTO.java
+++ src/main/java/com/umc/study/web/dto/ReviewRequestDTO.java
@@ -7,8 +7,12 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDate;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class ReviewRequestDTO {
 
@@ -27,5 +31,29 @@ public class ReviewRequestDTO {
 
         @NotBlank
         private Long storeId;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewListDTO{
+        List<ReviewPreViewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewDTO{
+        String ownerNickname;
+        Float score;
+        String body;
+        LocalDate createdAt;
     }
 }
